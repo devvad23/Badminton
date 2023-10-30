907e817b0815647b4a343866afd13cbe3859c7f4
devvad23 <122536340+devvad23@users.noreply.github.com>
2023-10-30 06:40:14 +0100

    Update Badminton_Stream.py

--- Badminton_Stream.py
+++ Badminton_Stream.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    d = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
+    d = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)
     
 
     st.balloons()
